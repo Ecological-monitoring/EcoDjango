2931fa4c1be63e304c96ef52aa4f0c35eada0a6c
Egorkud <egorkud2005@gmail.com>
2024-12-18 01:53:24 +0200

    Lab 1 v0.2

--- eco/urls.py
+++ eco/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.record_list, name='record_list'),  # Головна сторінка
-    path('create/', views.record_create, name='record_create'),  # Додати запис
+    path('', views.record_list, name='record_list'),
+    path('create/', views.record_create, name='record_create'),
+    path('edit/<int:pk>/', views.record_edit, name='record_edit'),
+    path('delete/<int:pk>/', views.record_delete, name='record_delete'),
 ]
--- eco/views.py
+++ eco/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 # Тут логіка основна
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import PollutionRecord
 from .forms import PollutionRecordForm
+from datetime import datetime, timedelta
 
 def record_list(request):
-    records = PollutionRecord.objects.all()
+    ten_years_ago = datetime.now().date() - timedelta(days=365*10)
+    records = PollutionRecord.objects.filter(date__gte=ten_years_ago)
     return render(request, 'record_list.html', {'records': records})
 
 def record_create(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = PollutionRecordForm(request.POST)
         if form.is_valid():
             form.save()
@@ -19,3 +21,21 @@ def record_create(request):
     else:
         form = PollutionRecordForm()
     return render(request, 'record_form.html', {'form': form})
+
+def record_edit(request, pk):
+    record = get_object_or_404(PollutionRecord, pk=pk)
+    if request.method == 'POST':
+        form = PollutionRecordForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            return redirect('record_list')
+    else:
+        form = PollutionRecordForm(instance=record)
+    return render(request, 'record_form.html', {'form': form})
+
+def record_delete(request, pk):
+    record = get_object_or_404(PollutionRecord, pk=pk)
+    if request.method == 'POST':
+        record.delete()
+        return redirect('record_list')
+    return render(request, 'record_confirm_delete.html', {'record': record})
